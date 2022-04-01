09bc44962ca11f62f35b8eeda0886905e68174ad
klem-bot-delta <54005826+klem-bot-delta@users.noreply.github.com>
2022-04-01 08:34:27 +0200

    Removed copy command from.

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 java -cp javacc.jar jjtree -tree src/com/company/griddy.jjt
 
-cp -r src/com/company/* src/com/company/
-
 java -cp javacc.jar javacc src/com/company/parser/griddy.jj
 
 javac -cp src src/com/company/Main.java
